94b02282016544849516a19f32fc3b8980ca5b44
FCTESLA <fcamacho888@gmail.com>
2020-09-21 23:42:27 -0700

    using javascript getElementByid, querySelectors and callback functions to calculate the tip amount and total amount

---
+++ script.js
@@ -0,0 +1,25 @@
+var tip = document.getElementById("tip");
+var bill = document.getElementById("bill");
+var submit = document.getElementById("submit");
+
+function calculateTip(total, tipPercent) {
+    var result = (total * tipPercent).toFixed(2);
+    return result;
+}
+
+function calculateTotal(total, tipAmount) {
+    return parseFloat(total) + parseFloat(tipAmount);
+  }
+
+function addTip() {
+    event.preventDefault();
+
+    var tipPercent = tip.value * .01;
+    var total = bill.value
+    var tipAmount = calculateTip(total, tipPercent);
+    var newTotal = calculateTotal(tipAmount, total);
+    document.querySelector("#tip-amount").textContent = tipAmount;
+    document.querySelector("#new-total").textContent = newTotal.toFixed(2);
+}
+
+submit.addEventListener("click", addTip);
