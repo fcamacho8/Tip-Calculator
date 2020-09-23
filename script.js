dc3f8ad4b4e4590ad77a21b736748188229e44aa
FCTESLA <fcamacho888@gmail.com>
2020-09-22 17:44:28 -0700

    added some css

--- script.js
+++ script.js
@@ -18,8 +18,8 @@ function addTip() {
     var total = bill.value
     var tipAmount = calculateTip(total, tipPercent);
     var newTotal = calculateTotal(tipAmount, total);
-    document.querySelector("#tip-amount").textContent = tipAmount;
-    document.querySelector("#new-total").textContent = newTotal.toFixed(2);
+    document.querySelector("#tip-amount").textContent = "$" + tipAmount;
+    document.querySelector("#new-total").textContent ="$" + newTotal.toFixed(2);
 }
 
 submit.addEventListener("click", addTip);
